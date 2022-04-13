257df4755cd7820c34b7477749a7c6cddaa33f09
Kel_BLK <70690676+Keltoum-BLK@users.noreply.github.com>
2022-04-13 12:12:15 +0200

    GoodAds Project created and added

---
+++ GoodAds/GoodAds/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GoodAds
+//
+//  Created by Kel_Jellysh on 13/04/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
